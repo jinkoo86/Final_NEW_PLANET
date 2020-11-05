8b43fa219d5797b01eb9ced841fff4ecc00b37b3
Jinkoo Kang <jinkoo86@gmail.com>
2020-11-05 17:13:52 +0900

    2020-11-05
    Update

---
+++ Assets/1.Script/KJK/Hammervfx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammervfx : MonoBehaviour
+{
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log("닿았다");
+    }
+}
