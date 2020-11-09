b113993465db7c0374329bcfc109d6fd29dc7469
Jinkoo Kang <jinkoo86@gmail.com>
2020-11-09 14:07:09 +0900

    2020-11-09
    combing with SEJ

---
+++ Assets/1.Script/SEJ/01.Script/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager soundMN;
+    private void Awake()
+    {
+        soundMN = this;
+
+    }
+    AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        sound.playOnAwake = false;
+
+        if (DB.instance.Volume == 0)
+        {
+            Debug.Log("DB.instance.Volume == " + DB.instance.Volume);
+            StopSound();
+        }
+        else
+        {
+            Debug.Log("DB.instance.Volume:" + DB.instance.Volume);
+            PlaySound();
+        }
+
+    }
+    public void PlaySound()
+    {
+        sound.Play();
+    }
+    public void StopSound()
+    {
+        sound.Stop();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
