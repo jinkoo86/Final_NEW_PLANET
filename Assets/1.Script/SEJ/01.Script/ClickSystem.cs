825570fa14c3c405457a652fa19c024af61e873a
leehb105 <leesonge95@gmail.com>
2020-11-09 21:55:42 +0900

    20201109
    씬 병합

--- Assets/1.Script/SEJ/01.Script/ClickSystem.cs
+++ Assets/1.Script/SEJ/01.Script/ClickSystem.cs
@@ -28,8 +28,8 @@ public class ClickSystem : MonoBehaviour
     void Start()
     {
         SetDBPath();
-        uiHelpers.GetComponent<LaserPointer>().enabled = false;
-        uiHelpers.GetComponent<LineRenderer>().enabled = false;
+        /*uiHelpers.GetComponent<LaserPointer>().enabled = false;
+        uiHelpers.GetComponent<LineRenderer>().enabled = false;*/
     }
 
     void Update()
@@ -89,8 +89,8 @@ public class ClickSystem : MonoBehaviour
     {
         menu.SetActive(false);
         //UIHelpers의 컴포넌트들을 disable하고싶다. 
-        uiHelpers.GetComponent<LaserPointer>().enabled = false;
-        uiHelpers.GetComponent<LineRenderer>().enabled = false;
+        /*uiHelpers.GetComponent<LaserPointer>().enabled = false;
+        uiHelpers.GetComponent<LineRenderer>().enabled = false;*/
     }
     //Waiting Room으로 버튼
     public void OnClickWaitingRoom()
