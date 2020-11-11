dea7c31535b48ab0d7c0eac2a54acebbb1cbf851
dongkeon95 <suntan1234@gmail.com>
2020-11-12 02:31:31 +0900

    20201112_pdk

--- Assets/1.Script/SEJ/01.Script/NMManager_StageRoom.cs
+++ Assets/1.Script/SEJ/01.Script/NMManager_StageRoom.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NMManager_StageRoom : MonoBehaviour
-{
+public class NMManager_StageRoom : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
-        SocketClient.instance.callGameFinish(ResultUI.instance.score.ToString("F0"));
-        
+    void Start() {
+        //SocketClient.instance.callGameFinish(ResultUI.instance.score.ToString("F0"));
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
