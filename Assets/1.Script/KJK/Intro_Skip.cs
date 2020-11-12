949b53f51ac05ee260a8e6903b2650d7e22332e1
Jinkoo Kang <jinkoo86@gmail.com>
2020-11-12 22:41:44 +0900

    2020-11-12

---
+++ Assets/1.Script/KJK/Intro_Skip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro_Skip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.Touch) || OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger, OVRInput.Controller.Touch) || OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.Touch) || OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger, OVRInput.Controller.Touch))
+
+        {
+            SceneManager.LoadScene("WaitingRoom");
+        }
+    }
+}
